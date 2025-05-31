6f98ce9d959912f0e58182ad78db5f08ba2aa06b
Cheerios4316 <accfake7214@gmail.com>
2025-05-31 22:14:22 +0200

    update import in template

--- .dumpsterfire-template/src/Components/Pages/MainPageComponent/MainPageComponent.php
+++ .dumpsterfire-template/src/Components/Pages/MainPageComponent/MainPageComponent.php
@@ -2,6 +2,7 @@
 
 namespace Src\Components\Pages\MainPageComponent;
 
+use DumpsterfirePages\Interfaces\ISetup;
 use DumpsterfirePages\PageComponent;
 
 class MainPageComponent extends PageComponent implements ISetup
