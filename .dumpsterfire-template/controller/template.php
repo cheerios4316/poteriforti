2b8c15cf48e027df4663470ba3dd579b93a91b25
egidio.pignataro <egidio.pignataro@mavriq.biz>
2025-08-18 12:50:08 +0200

    add controller template

---
+++ .dumpsterfire-template/controller/template.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Src\Controllers;
+
+use DumpsterfirePages\Controller\BaseController;
+use DumpsterfirePages\PageComponent;
+
+class /*[{name}]*/ extends BaseController
+{
+    public function getResult(): PageComponent
+    {
+        // @todo return a page
+    }
+}
+
